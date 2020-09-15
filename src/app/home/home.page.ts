2b4983716e6e4e1f6d3b43d8db1884214e6e05bb
Allison Espinoza <alleespi@espol.edu.ec>
2020-09-14 22:49:49 -0500

    arcore

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,23 @@
+import { Component,  OnInit, ViewEncapsulation } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class HomePage implements OnInit {
+
+  constructor(private modalCtrl: ModalController) {}
+
+
+  ngOnInit() {
+
+  }
+
+  close(){
+    this.modalCtrl.dismiss();
+  }
+
+}
